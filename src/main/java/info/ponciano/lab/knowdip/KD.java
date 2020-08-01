7b86470199cf0295fbe9ce4090fb80131cca9534
Jean-Jacques Ponciano <ponciano.jeanjacques@gmail.com>
2020-08-01 10:44:01 +0200

    Create KD.java

---
+++ src/main/java/info/ponciano/lab/knowdip/KD.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2020 Dr Jean-Jacques Ponciano <jean-jacques@ponciano.info>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package info.ponciano.lab.knowdip;
+
+/**
+ *
+ * @author Dr Jean-Jacques Ponciano <jean-jacques@ponciano.info>
+ */
+public class KD {
+
+    public static final String URI = "http://lab.ponciano.info/knowdip";
+    public static final String NS = URI + "#";
+    public static final String ALGORITHM = NS + "Algorithm";
+    public static String OBJECT = NS + "Object";
+    public static String HAS_VALUE = NS + "hasValue";
+    public static String HAS_CHARA = NS + "hasChara";
+    public static String HAS_INPUT = NS + "hasInput";
+    public static String HAS_OUTPUT = NS + "hasOutput";
+    public static String HAS_DATA_OUTPUT = NS + "hasDataOutput";
+    public static String IS_RELEVANT_TO = NS + "isRelevantTo";
+    public static String NOTHING = NS + "nothing";
+
+}
