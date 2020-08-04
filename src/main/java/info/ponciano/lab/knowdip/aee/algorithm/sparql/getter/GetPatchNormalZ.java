0fca1fb408c009731ec90c38634cabc5034d673e
Jean-Jacques Ponciano <ponciano.jeanjacques@gmail.com>
2020-08-04 17:00:17 +0200

    Create GetPatchNormalZ.java

---
+++ src/main/java/info/ponciano/lab/knowdip/aee/algorithm/sparql/getter/GetPatchNormalZ.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2020 Dr Jean-Jacques Ponciano <jean-jacques@ponciano.info>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package info.ponciano.lab.knowdip.aee.algorithm.sparql.getter;
+
+import info.ponciano.lab.jpc.pointcloud.Pointcloud;
+import info.ponciano.lab.jpc.pointcloud.components.APointCloud;
+import info.ponciano.lab.knowdip.aee.KnowdipException;
+import info.ponciano.lab.knowdip.aee.algorithm.sparql.Algorithm;
+import info.ponciano.lab.knowdip.aee.memory.WritableResource;
+import java.util.Collections;
+import org.apache.jena.datatypes.xsd.XSDDatatype;
+import org.apache.jena.graph.Node;
+import org.apache.jena.graph.NodeFactory;
+
+/**
+ *
+ * @author Dr Jean-Jacques Ponciano <jean-jacques@ponciano.info>
+ */
+public class GetPatchNormalZ extends Algorithm {
+
+    WritableResource hasInput;
+    String patchID;
+
+    @Override
+    protected Iterable<Node> process() throws KnowdipException {
+        Object data = hasInput.getData();
+        if (!data.getClass().equals(Pointcloud.class)) {
+            System.err.println("hasInput is not a patch in "+getClass()+"");
+        } else {
+            Pointcloud pc = (Pointcloud) data;
+            String localName =patchID.substring(patchID.lastIndexOf('#')+1,patchID.length());
+            APointCloud patch = pc.get(localName);
+            double value = patch.getMeanNormal().getZ();
+            
+            return Collections.singleton(NodeFactory.createLiteralByValue(value,
+                    XSDDatatype.XSDinteger));
+        }
+        return null;
+    }
+}
