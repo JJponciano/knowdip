563ce5d8a3fd84760cd82087bfdd742bd5b7775b
Jean-Jacques Ponciano <ponciano.jeanjacques@gmail.com>
2020-08-05 11:06:15 +0200

    Create PiOntologyException.java
    Remove PiLib

---
+++ src/main/java/info/ponciano/lab/knowdip/reasoner/PiOntologyException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.ponciano.lab.knowdip.reasoner;
+
+/**
+ *
+ * @author LeVic
+ */
+public class PiOntologyException extends Exception {
+
+    public PiOntologyException(String string) {
+    }
+    
+}
