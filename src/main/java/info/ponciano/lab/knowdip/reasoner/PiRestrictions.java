9c20db4fb3918f2718be943e45339c6ad67a15d7
Jean-Jacques Ponciano <ponciano.jeanjacques@gmail.com>
2020-08-05 11:14:39 +0200

    Update PiRestrictions.java

--- src/main/java/info/ponciano/lab/knowdip/reasoner/PiRestrictions.java
+++ src/main/java/info/ponciano/lab/knowdip/reasoner/PiRestrictions.java
@@ -1,7 +1,18 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2020 Dr Jean-Jacques Ponciano <jean-jacques@ponciano.info>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package info.ponciano.lab.knowdip.reasoner;
 
@@ -10,7 +21,7 @@ import java.util.List;
 
 /**
  *
- * @author jean-jacques
+ * @author jean-jacques ponciano
  */
 public class PiRestrictions {
 
