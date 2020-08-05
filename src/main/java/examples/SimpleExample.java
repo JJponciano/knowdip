f7b85d3aca50fd9ef2253715b1b18baa196abf03
Jean-Jacques Ponciano <ponciano.jeanjacques@gmail.com>
2020-08-05 11:05:26 +0200

    Update SimpleExample.java

--- src/main/java/examples/SimpleExample.java
+++ src/main/java/examples/SimpleExample.java
@@ -21,7 +21,8 @@ import info.ponciano.lab.knowdip.aee.KnowdipException;
 import info.ponciano.lab.knowdip.aee.algorithm.sparql.LoadCloud;
 import info.ponciano.lab.knowdip.aee.algorithm.sparql.PatchesSegmentation;
 import info.ponciano.lab.knowdip.aee.algorithm.sparql.getter.GetPatchSize;
-import info.ponciano.lab.pisemantic.PiOntologyException;
+import info.ponciano.lab.knowdip.reasoner.PiOntologyException;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -36,11 +37,17 @@ public class SimpleExample {
 
     public static void main(String[] args) {
 
-        args = new String[2];
-        args[0] = "src/main/resources/knowdip.owl";
-        args[1] = "output/";
         try {
-            Knowdip.init(args[0], args[1], true);
+            args = new String[2];
+            args[0] = "src/main/resources/knowdip.owl";
+            args[1] = "output/";
+            try {
+                Knowdip.init(args[0], args[1], true);
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(SimpleExample.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (PiOntologyException ex) {
+                Logger.getLogger(SimpleExample.class.getName()).log(Level.SEVERE, null, ex);
+            }
             Knowdip knowdip = Knowdip.get();
             knowdip.add(LoadCloud.class);
             knowdip.add(PatchesSegmentation.class);
@@ -50,32 +57,30 @@ public class SimpleExample {
                     + "?i0 rdf:type knowdip:PointCloudFile . "
                     + "FILTER NOT EXISTS { ?something knowdip:readFrom ?i0 } . " + "?i0 knowdip:hasSource ?v0 ."
                     + "?out knowdip:LoadCloud( \"hasSource =\" ?v0)" + "}");
-
             // Segments in patches point cloud
             knowdip.interprets("CONSTRUCT{ ?out rdf:type knowdip:Patches . ?out knowdip:comesFrom ?i0.} " + "WHERE{"
                     + "?i0 rdf:type knowdip:FullPointCloud . "
                     + "FILTER NOT EXISTS { ?something knowdip:comesFrom ?i0 } . "
                     + "?out knowdip:PatchesSegmentation( \"hasInput =\" ?i0)"
                     + "}");
-
-            // Get the size of each patch 
+            // Get the size of each patch
             knowdip.interprets("CONSTRUCT{ ?p knowdip:hasSize ?out } " + "WHERE{"
                     + "?p rdf:type knowdip:Patches . "
                     + "FILTER NOT EXISTS { ?p knowdip:hasSize ?s } . "
                     + "?p knowdip:comesFrom ?i0 . "
                     + "?out knowdip:GetPatchSize( \"hasInput =\" ?i0 \"patchID =\" ?p)"
                     + "}");
-
             ResultSet select = knowdip.select("SELECT ?c WHERE{ ?c rdf:type knowdip:FullPointCloud }");
             while (select.hasNext()) {
                 QuerySolution next = select.next();
                 String uri = next.get("c").asResource().getURI();
                 System.out.println(uri);
             }
-
             knowdip.save();
-        } catch (KnowdipException | IOException | PiOntologyException ex) {
-            Logger.getLogger(Knowdip.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(SimpleExample.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (KnowdipException ex) {
+            Logger.getLogger(SimpleExample.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
