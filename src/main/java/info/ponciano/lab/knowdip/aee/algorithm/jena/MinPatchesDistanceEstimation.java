4926c9051c62addf5191431579145e68d2004b29
Jean-Jacques Ponciano <ponciano.jeanjacques@gmail.com>
2020-08-10 09:55:52 +0200

    Add getPatches method

--- src/main/java/info/ponciano/lab/knowdip/aee/algorithm/jena/MinPatchesDistanceEstimation.java
+++ src/main/java/info/ponciano/lab/knowdip/aee/algorithm/jena/MinPatchesDistanceEstimation.java
@@ -16,17 +16,39 @@
  */
 package info.ponciano.lab.knowdip.aee.algorithm.jena;
 
+import info.ponciano.lab.jpc.pointcloud.components.APointCloud;
+import info.ponciano.lab.knowdip.Knowdip;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
+
 /**
- *
+ *{@code MinPatchesDistanceEstimation } aims to calculate the minimum distance between all patches stored in the ontology according to their oriented bounding box.
  * @author Dr Jean-Jacques Ponciano <jean-jacques@ponciano.info>
  */
-public class MinPatchesDistanceEstimation {
+public class MinPatchesDistanceEstimation implements Runnable{
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    @Override
+    public void run() {
+        
+        getPatches();
+        
     }
+
+    private void getPatches() {
+        //get all patches
+        List<APointCloud> patches=new ArrayList<>();
+        ResultSet select = Knowdip.get().select("SELECT ?p WHERE{ ?p rdf:type knowdip:Patch}");
+        while (select.hasNext()) {
+            //get URI of the patch
+            QuerySolution next = select.next();
+            String uri = next.get("p").asResource().getURI();
+            //retrieve the patch in the memory
+            APointCloud access = (APointCloud) Knowdip.get().getMemory().access(uri);
+            patches.add(access);
+        }
+    }
+
     
 }
