50a840857895587839ff6c4b5e72d7f464fc3722
Jean-Jacques Ponciano <ponciano.jeanjacques@gmail.com>
2020-08-19 10:34:48 +0200

    Create TwoVar.java

---
+++ src/main/java/info/ponciano/lab/knowdip/reasoner/TwoVar.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2020 Dr Jean-Jacques Ponciano <jean-jacques@ponciano.info>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package info.ponciano.lab.knowdip.reasoner;
+
+/**
+ *
+ * @author Dr Jean-Jacques Ponciano <jean-jacques@ponciano.info>
+ */
+public class TwoVar {
+    
+    String v1;
+    String v2;
+
+    public TwoVar(String v1, String v2) {
+        this.v1 = v1;
+        this.v2 = v2;
+    }
+
+    public String getV1() {
+        return v1;
+    }
+
+    public String getV2() {
+        return v2;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (obj.getClass() != this.getClass() && this == obj) {
+            return true;
+        }
+
+        try {
+            final TwoVar other = (TwoVar) obj;
+
+            return (other.getV1().equals(v1) && other.getV2().equals(v2)) || (other.getV2().equals(v1) && other.getV1().equals(v2));
+
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+}
