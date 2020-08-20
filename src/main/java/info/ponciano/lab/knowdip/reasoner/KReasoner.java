838af12ac25a76a1c04c3ae42fe82975337d5c74
Jean-Jacques Ponciano <ponciano.jeanjacques@gmail.com>
2020-08-20 11:13:04 +0200

    Update KReasoner.java

--- src/main/java/info/ponciano/lab/knowdip/reasoner/KReasoner.java
+++ src/main/java/info/ponciano/lab/knowdip/reasoner/KReasoner.java
@@ -16,14 +16,10 @@
  */
 package info.ponciano.lab.knowdip.reasoner;
 
-import info.ponciano.lab.knowdip.KD;
 import info.ponciano.lab.knowdip.aee.KnowdipException;
 import info.ponciano.lab.knowdip.aee.algorithm.sparql.Algorithm;
 import info.ponciano.lab.knowdip.aee.memory.Memory;
 import info.ponciano.lab.knowdip.reasoner.automatic.PiRegex;
-import info.ponciano.lab.knowdip.reasoner.automatic.SemObject;
-import info.ponciano.lab.knowdip.reasoner.automatic.algorithms.SparqlAlgorithm;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -33,7 +29,6 @@ import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.apache.jena.datatypes.RDFDatatype;
-import org.apache.jena.ontology.OntClass;
 import org.apache.jena.ontology.OntModel;
 import org.apache.jena.query.QuerySolution;
 import org.apache.jena.query.ResultSet;
@@ -44,64 +39,14 @@ import org.apache.jena.rdf.model.RDFNode;
  *
  * @author Jean-Jacques Ponciano
  */
-public class KReasoner {
+public abstract class KReasoner {
 
-    protected PiOnt piont;
-    protected List<OntClass> objects;
-    protected Kee kee;
+    protected abstract Kee getKee();
 
-    public KReasoner(Kee kee) throws FileNotFoundException, PiOntologyException {
-        this.kee = kee;
-        this.piont = new PiOnt(this.kee.getWorkingModel());
-        //select every subclass of object
-        OntClass objectCl = piont.getOntClass(KD.OBJECT);
-        this.objects = piont.getSubClass(objectCl, true);
-    }
-
-    private void inferRoot() throws KnowdipException {
-        this.kee.construct("CONSTRUCT { ?x rdf:type ?sub } WHERE {"
+    public void inferRoot() throws KnowdipException {
+        this.getKee().construct("CONSTRUCT { ?x rdf:type ?sub } WHERE {"
                 + "?x rdf:type ?t . ?t rdfs:subClassOf ?sub . "
                 + "} ");
-    }
-
-    /**
-     * Execute algorithms by translate algorithms information from ontology to
-     * SPARQL queries
-     *
-     * @throws KnowdipException
-     * @deprecated it is more robust to use @code{interprets()}
-     * @see KReasoner.interprets
-     */
-    public void genericExecution() throws KnowdipException {
-        try {
-            //get the algorithm class
-            OntClass algoOC = piont.getOntClass(KD.ALGORITHM);
-            //get all subclass of algorithm
-            List<OntClass> algos = piont.getSubClass(algoOC, true);
-            //for each algorithm's subclass
-            boolean construct = true;
-            while (construct) {
-                this.inferRoot();//infer the ontology with root rules
-                construct = false;
-                for (OntClass algo : algos) {
-                    //build semantically the algorithm
-                    SparqlAlgorithm semAlgo = new SparqlAlgorithm(algo, piont);
-                    //get update if it exists
-                    List<String> updateQuery = getUpdateQuery(semAlgo);
-                    construct |= !updateQuery.isEmpty();
-                    updateQuery.forEach((query) -> {
-
-                        try {
-                            this.kee.update(query);
-                        } catch (KnowdipException ex) {
-                            Logger.getLogger(KReasoner.class.getName()).log(Level.SEVERE, null, ex);
-                        }
-                    });
-                }
-            }
-        } catch (PiOntologyException ex) {
-            Logger.getLogger(KReasoner.class.getName()).log(Level.SEVERE, null, ex);
-        }
     }
 
     /**
@@ -123,7 +68,7 @@ public class KReasoner {
         updateQuery.forEach((query) -> {
             try {
                 //                 System.out.println(query);
-                this.kee.update(query);
+                this. getKee().update(query);
             } catch (KnowdipException ex) {
                 Logger.getLogger(KReasoner.class.getName()).log(Level.SEVERE, null, ex);
             }
@@ -145,55 +90,9 @@ public class KReasoner {
         return res;
     }
 
-    /**
-     * Extract SParql construct from object description inside the ontology. Do
-     * not yet support OWL
-     *
-     * @deprecated better to use direct SPARQL CONSTRUCT;
-     * @throws KnowdipException
-     * @throws PiOntologyException
-     */
-    public void genericClassification() throws KnowdipException, PiOntologyException {
-        //for each class and up to idempotence
-        boolean construct = true;
-        while (construct) {
-            construct = false;
-            //for each object, get all restriction
-            for (OntClass o : objects) {
-                //build semantically the object
-                SemObject semObject = new SemObject(o, piont);
-                //select every individual corresponding to the restriction and
-                //classify individuals as the object
-                String classifQuery = semObject.getClassif();
-                if (!classifQuery.isEmpty()) {
-//                    System.out.println("***************");
-//                    System.out.println(classifQuery);
-                    construct |= this.kee.construct(classifQuery);
-                }
-            }
-        }
-    }
-
-    private List<String> getUpdateQuery(SparqlAlgorithm semAlgo) throws KnowdipException {
-
-        String selectQuery = semAlgo.getSelect();
-        String selectOut = semAlgo.getExec();
-
-        String maj = semAlgo.getInsert();
-//        System.out.println("---------------------------------------------\n"
-//                + selectQuery + "*********************\n"
-//                + selectOut + "*********************\n"
-//                + "\n" + maj);
-
-//        System.out.println("vars: ");
-        List<String> vars = semAlgo.getVarSelected();
-
-        return getInsert(selectQuery, vars, selectOut, maj);
-    }
-
     private List<String> getInsert(String selectQuery, List<String> vars, String selectOut, String maj) {
         List<String> updateQuery = new LinkedList<>();
-        ResultSet select = this.kee.select(selectQuery);
+        ResultSet select = this. getKee().select(selectQuery);
         //select all the variables needed
 
         List<String> executeMemory = new LinkedList<>();
@@ -219,7 +118,7 @@ public class KReasoner {
             //Executes algorithms 
             if (!executeMemory.contains(sout)) {
                 executeMemory.add(sout);
-                ResultSet selectOutrs = this.kee.select(sout);
+                ResultSet selectOutrs = this. getKee().select(sout);
                 //retrieve the result.
                 while (selectOutrs.hasNext()) {
                     QuerySolution nextOut = selectOutrs.nextSolution();
@@ -334,7 +233,7 @@ public class KReasoner {
         String var = reg.getGroup(1).get(0);
         query = query.replaceFirst("REMOVE|remove", "SELECT");
 
-        ResultSet select = this.kee.select(query);
+        ResultSet select = this. getKee().select(query);
         List<String> uris = new ArrayList();
         while (select.hasNext()) {
             QuerySolution next = select.next();
@@ -342,7 +241,7 @@ public class KReasoner {
             uris.add(get.asResource().getURI());
         }
         for (String uri : uris) {
-            this.kee.update("DELETE WHERE {<" + uri + "> ?p ?o}");
+            this. getKee().update("DELETE WHERE {<" + uri + "> ?p ?o}");
         }
 
     }
@@ -354,39 +253,35 @@ public class KReasoner {
         return sout.replaceAll(exp, string);
     }
 
-    public PiOnt getPiont() {
-        return piont;
-    }
-
     public Memory getMemory() {
-        return this.kee.getMemory();
+        return this. getKee().getMemory();
     }
 
     public void add(Class<? extends Algorithm> algo) throws KnowdipException {
-        this.kee.add(algo);
+        this. getKee().add(algo);
     }
 
     public void update(String query) throws KnowdipException {
-        this.kee.update(query);
+        this. getKee().update(query);
     }
 
     public boolean construct(String queryString) throws KnowdipException {
-        return this.kee.construct(queryString);
+        return this. getKee().construct(queryString);
     }
 
     public void close() {
-        this.kee.close();
+        this. getKee().close();
     }
 
     public ResultSet select(String query) {
-        return this.kee.select(query);
+        return this. getKee().select(query);
     }
 
     public String selectAsText(String query) {
-        return this.kee.selectAsText(query);
+        return this. getKee().selectAsText(query);
     }
 
     public OntModel getModel() {
-        return this.kee.getModel();
+        return this. getKee().getModel();
     }
 }
