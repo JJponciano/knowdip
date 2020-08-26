6a6610f94621215dfc3ae263d5125637d465af47
Jean-Jacques Ponciano <ponciano.jeanjacques@gmail.com>
2020-08-26 09:57:12 +0200

    Interpret file

--- src/main/java/examples/SemanticSegmentationExample.java
+++ src/main/java/examples/SemanticSegmentationExample.java
@@ -16,6 +16,28 @@
  */
 package examples;
 
+import info.ponciano.lab.knowdip.Knowdip;
+import info.ponciano.lab.knowdip.aee.KnowdipException;
+import info.ponciano.lab.knowdip.aee.algorithm.sparql.LoadCloud;
+import info.ponciano.lab.knowdip.aee.algorithm.sparql.PatchesSegmentation;
+import info.ponciano.lab.knowdip.aee.algorithm.sparql.getter.GetPatchArea;
+import info.ponciano.lab.knowdip.aee.algorithm.sparql.getter.GetPatchColor;
+import info.ponciano.lab.knowdip.aee.algorithm.sparql.getter.GetPatchDensity;
+import info.ponciano.lab.knowdip.aee.algorithm.sparql.getter.GetPatchDistanceX;
+import info.ponciano.lab.knowdip.aee.algorithm.sparql.getter.GetPatchDistanceY;
+import info.ponciano.lab.knowdip.aee.algorithm.sparql.getter.GetPatchDistanceZ;
+import info.ponciano.lab.knowdip.aee.algorithm.sparql.getter.GetPatchMaxZ;
+import info.ponciano.lab.knowdip.aee.algorithm.sparql.getter.GetPatchMinZ;
+import info.ponciano.lab.knowdip.aee.algorithm.sparql.getter.GetPatchNormalX;
+import info.ponciano.lab.knowdip.aee.algorithm.sparql.getter.GetPatchNormalY;
+import info.ponciano.lab.knowdip.aee.algorithm.sparql.getter.GetPatchNormalZ;
+import info.ponciano.lab.knowdip.aee.algorithm.sparql.getter.GetPatchSize;
+import info.ponciano.lab.knowdip.aee.algorithm.sparql.getter.GetPatchVolume;
+import info.ponciano.lab.knowdip.reasoner.PiOntologyException;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Dr Jean-Jacques Ponciano <jean-jacques@ponciano.info>
@@ -26,7 +48,38 @@ public class SemanticSegmentationExample {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            args = new String[2];
+            args[0] = "src/main/resources/knowdip.owl";
+            args[1] = "output/";
+            /**
+             * ontologyPath,output directory, reset the output directory, using
+             * a triple store
+             */
+            Knowdip.init(args[0], args[1], false, true);
+
+            Knowdip knowdip = Knowdip.get();
+            knowdip.add(LoadCloud.class);
+            knowdip.add(PatchesSegmentation.class);
+            knowdip.add(GetPatchSize.class);
+            knowdip.add(GetPatchColor.class);
+            knowdip.add(GetPatchArea.class);
+            knowdip.add(GetPatchDensity.class);
+            knowdip.add(GetPatchDistanceX.class);
+            knowdip.add(GetPatchDistanceY.class);
+            knowdip.add(GetPatchDistanceZ.class);
+            knowdip.add(GetPatchMaxZ.class);
+            knowdip.add(GetPatchMinZ.class);
+            knowdip.add(GetPatchNormalX.class);
+            knowdip.add(GetPatchNormalY.class);
+            knowdip.add(GetPatchNormalZ.class);
+            knowdip.add(GetPatchVolume.class);
+
+            //interprets all SPARQL queries contained in the file
+            knowdip.interpretsFile("src/main/resources/queries.txt");
+        } catch (IOException | KnowdipException | PiOntologyException ex) {
+            Logger.getLogger(SemanticSegmentationExample.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
+
 }
